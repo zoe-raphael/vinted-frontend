9edf416c42f799dc872e21ae8f47364348331caa
unknown <zoeraphael@hotmail.com>
2025-03-13 17:00:14 +0100

    formulaire ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import Signup from "./Signup";
+// import Signup from "./Signup";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Header = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post(
-          "https://lereacteur-vinted-api.herokuapp.com/user/signup"
-        );
-        setData(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.post(
+  //         "https://lereacteur-vinted-api.herokuapp.com/user/signup"
+  //       );
+  //       setData(response.data);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="wrapper">
@@ -38,27 +38,13 @@ const Header = () => {
         <div className="sign">
           {/* <button
             onClick={async (event) => {
-              try {
-                event.preventDefault();
-                const response = await axios.post(
-                  "https://lereacteur-vinted-api.herokuapp.com/v2/offers",
-                  {
-                    email: "johndoe@lereacteur.io",
-                    username: "JohnDoe",
-                    password: "azerty",
-                    newsletter: true,
-                  }
-                );
-                Cookies.set("userToken", response.data.token);
-                setToken(response.data.token);
-                navigate("/signup");
-              } catch (error) {
-                alert(error.response);
-              }
+             
             }}
-          > */}
-          <button onClick={navigate("/signup")}>S'inscrire</button>
-          <button>Se connecter</button>
+          >
+             <button onClick={navigate("/signup")}>S'inscrire</button> 
+          </button>  */}
+          <Link to={"/signup"}>Se connecter</Link>
+
           <button>Vends tes articles</button>
         </div>
       </header>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,12 +1,51 @@
-import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Signup = () => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const submitForm = async () => {
+    try {
+      e.preventDefault();
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+        {
+          email: data.email,
+          username: data.account.username,
+          newsletter: data.newsletter,
+          token: data.token,
+        }
+      );
+      Cookies.set("userToken", response.data.token);
+      setToken(response.data.token);
+      navigate("/signup");
+    } catch (error) {
+      alert(error.response);
+    }
+  };
 
   return (
     <div>
-      <div>signup</div>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Nom d'utilisateur"
+        />
+        <input type="email" name="email" id="email" placeholder="Email" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Mot de passe"
+        />
+        <input type="checkbox" name="newsletter" id="newsletter" />
+        <p>S'inscrire à la newsletter</p>
+        <button id="submit-btn" type="submit">
+          S'inscrire
+        </button>
+        {/* <Link to={"/connect"}>Connection</Link> */}
+      </form>
     </div>
   );
 };
