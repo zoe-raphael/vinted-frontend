354604d1286e055d13a0101d30a02385394f3ff7
unknown <zoeraphael@hotmail.com>
2025-03-13 17:57:14 +0100

    form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,28 +5,52 @@ import Cookies from "js-cookie";
 
 const Signup = () => {
   const [token, setToken] = useState(Number(Cookies.get("token")) || 0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [newsletter, setnewsletter] = useState(false);
 
-  const submitForm = async () => {
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleNewsletterChange = (event) => {
+    const value = event.target.value;
+    setnewsletter(value);
+  };
+
+  const handleUsernameChange = (event) => {
+    const value = event.target.value;
+    setUsername(value);
+  };
+
+  const submitForm = async (e) => {
     try {
       e.preventDefault();
       const response = await axios.post(
         "https://lereacteur-vinted-api.herokuapp.com/user/signup",
         {
-          email: data.email,
-          username: data.account.username,
-          newsletter: data.newsletter,
+          email: response.data.email,
+          username: response.data.account.username,
+          newsletter: response.data.newsletter,
+          password: response.data.password,
           //   token: data.token,
         }
       );
 
-      Cookies.set("token", { expires: 3 });
+      Cookies.set("token", token, { expires: 3 });
       setToken(response.data.token);
 
-      //   Cookies.set("userToken", response.data.token);
-      //   setToken(response.data.token);
-      navigate("/signup");
+      navigate("/home");
     } catch (error) {
-      alert(error.response);
+      alert(error);
+      //   error.message;
     }
   };
 
@@ -34,19 +58,36 @@ const Signup = () => {
     <div>
       <form onSubmit={submitForm}>
         <input
+          value={username}
           type="text"
           name="username"
           id="username"
           placeholder="Nom d'utilisateur"
+          onChange={handleUsernameChange}
         />
-        <input type="email" name="email" id="email" placeholder="Email" />
         <input
+          value={email}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onChange={handleEmailChange}
+        />
+        <input
+          value={password}
           type="password"
           name="password"
           id="password"
           placeholder="Mot de passe"
+          onChange={handlePasswordChange}
         />
-        <input type="checkbox" name="newsletter" id="newsletter" />
+        <input
+          value={newsletter}
+          type="checkbox"
+          name="newsletter"
+          id="newsletter"
+          onChange={handleNewsletterChange}
+        />
         <p>S'inscrire à la newsletter</p>
         <button id="submit-btn" type="submit">
           S'inscrire
